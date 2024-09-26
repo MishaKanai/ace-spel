53ee629422b5e608a2c9a91287a8a85a1cf72358
Misha Kanai <mkanai@ssg-llc.com>
2024-09-26 19:53:12 -0400

    Write package.json in directory we create and put ace-builds inside of. Hopefully helps vite with imports

---
+++ writePackageJSON.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+// Example package.json content
+const packageJsonContent = {
+  name: "@ace-spel",
+  version: "0.0.1",
+  exports: {
+    ".*": "./ace-builds/src-noconflict/*.js"
+  }
+};
+
+// Function to write package.json to the specified file path
+function writePackageJson(filePath) {
+  const fullPath = path.resolve(filePath, 'package.json');
+  
+  fs.writeFile(fullPath, JSON.stringify(packageJsonContent, null, 2), (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return;
+    }
+    console.log('package.json has been written to', fullPath);
+  });
+}
+
+// Take the file path from the command line arguments
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error('Please provide a file path as the first argument.');
+  process.exit(1);
+}
+
+writePackageJson(filePath);
